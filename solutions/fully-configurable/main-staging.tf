eadc5c8de145b95eb433b5d66c0b03afa46956e4
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-03-13 15:33:12 -0400

    read ssh keys list to validate api key by doing a login. update da to match prod

--- solutions/fully-configurable/main-staging.tf
+++ solutions/fully-configurable/main-staging.tf
@@ -1,3 +1,15 @@
+# read ssh keys available in the cloud under this account - don't care
+# about the keys just want to force the terraform to login to the cloud.
+data "ibm_is_ssh_keys" "mykeys" {
+}
+
+locals {
+    howmanykeys = length(data.ibm_is_ssh_keys.mykeys.keys)
+}
+output "mykeys" {
+    value = "There are ${local.howmanykeys} ssh keys defined by the account owning this api key."
+}
+
 resource "time_sleep" "wait_5_seconds" {
     create_duration = "5s"
 }
